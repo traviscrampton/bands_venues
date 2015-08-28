7b1998f07cce31a8684b39f726e9641e7a19bbef
Travis Crampton <crampton.travis@gmail.com>
2015-08-28 13:09:23 -0700

    added the scope

--- lib/band.rb
+++ lib/band.rb
@@ -1,8 +1,14 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many(:venues)
-
   validates(:name, :presence => true)
-scope(:not_done, -> do
-  where({:done => false})
-end)
+  before_save(:capitalize_name)
+
+  scope(:not_done, -> do
+    where({:done => false})
+  end)
+
+  private
+  define_method(:capitalize_name) do
+    self.name = name().capitalize()
+  end
 end
