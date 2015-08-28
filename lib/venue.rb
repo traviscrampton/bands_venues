e0cf6d33f65ff430c5ad9db40f07cf3b11fb6c20
Travis Crampton <crampton.travis@gmail.com>
2015-08-28 13:10:02 -0700

    add the scope

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,8 +1,13 @@
 class Venue < ActiveRecord::Base
   has_and_belongs_to_many(:bands)
-
   validates(:title, :presence => true)
+  before_save(:capitalize_title)
 scope(:not_done, -> do
   where({:done => false})
 end)
+
+private
+  define_method(:capitalize_title) do
+    self.title = title().capitalize()
+  end
 end
